4d37cddabee94477f3af5a42c1e357e7c632c1b6
quoctoank4it <quoctoank4it@gmail.com>
2024-11-26 21:02:07 +0700

    edit file

--- src/Components/PersonDetail/index.js
+++ src/Components/PersonDetail/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import data from "../../data.json";
 
 const findPersonById = (data, id) => {
@@ -14,12 +14,17 @@ const findPersonById = (data, id) => {
 };
 
 const PersonDetail = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const person = findPersonById(data.data, id);
 
   if (!person) {
     return <p>Không tìm thấy thông tin chi tiết cho ID: {id}</p>;
   }
+
+  const handleBack = () => {
+    navigate(-1); // Chuyển về trang trước đó
+  };
 
   return (
     <div className="content">
@@ -39,8 +44,12 @@ const PersonDetail = () => {
           {person.details?.w || person.details?.h || "Không có"}
         </p>
         <p>
-          <strong>Thông tin thêm:</strong>{" "}
+          <strong>Địa chỉ:</strong>{" "}
           {person.details?.additionalInfo || "Không có"}
+        </p>
+        <p>
+          <strong>Số điện thoại:</strong>{" "}
+          {person.details?.phonenumber || "Không có"}
         </p>
       </div>
     </div>
